583bda07b6b235f9d81c46b728bbe5b8e85819ad
Jbrak <joe@brakoniecki.com>
2020-09-30 09:49:51 +0100

    Added Random number guesser game.

---
+++ random_number_guesser.py
@@ -0,0 +1,20 @@
+from random import randint
+repeat = "Y"
+
+while repeat.upper() == "Y":
+    rand_num = randint(0,10)
+
+    print("\n", rand_num)
+
+    guess = int(input("\nMy Number is between 0-10. Take a Guess\n"))
+
+    while guess != rand_num:
+        guess = int(input("Incorrect. Take another guess:\n" ))
+        if guess > rand_num:
+            print("Your Number is too big ")
+        elif guess < rand_num:
+            print("Your Number is too low ")
+
+
+    print("Correct")
+    repeat = input("Do you want to take another go? Y/N\n")
