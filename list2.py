3c2587f08854ac158e42a76157eb6cdf184be4a9
Jbrak <joe@brakoniecki.com>
2020-11-16 16:58:43 +0000

    Added inputs

--- list2.py
+++ list2.py
@@ -1,8 +1,9 @@
-lst1 = [1,2,3,4,5,6]
-lst2 = [1,2,3,4,5,6]
+lst1 = [int(input("enter a number: ")) for i in range(0,9)]
+lst2 = [int(input("enter a number: ")) for i in range(0,9)]
 
 lst3 = [lst1[i] + lst2[i] for i in range(0,len(lst1))]
 lst3.sort()
 
+print(lst3)
 print("Largest:", lst3[-1])
 print("Smallest:", lst3[0])
