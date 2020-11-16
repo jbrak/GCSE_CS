37b5d5ce24ffc7eb15ed9902c19615183be6d594
Jbrak <joe@brakoniecki.com>
2020-11-16 21:30:16 +0000

    updated

--- list2.py
+++ list2.py
@@ -2,6 +2,9 @@ lst1 = [int(input("enter a number: ")) for i in range(0,9)]
 lst2 = [int(input("enter a number: ")) for i in range(0,9)]
 
 lst3 = [lst1[i] + lst2[i] for i in range(0,len(lst1))]
+
+print(lst3)
+
 lst3.sort()
 
 print(lst3)
