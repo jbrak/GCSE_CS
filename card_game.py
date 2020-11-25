e3ea1b95aa8f497ac2eb2288ebcc069d3c8c0016
Jbrak <joe@brakoniecki.com>
2020-11-25 10:33:50 +0000

    CREATED

---
+++ card_game.py
@@ -0,0 +1,27 @@
+from random import randint
+from time import sleep
+cards = []
+for i in range(1,31):
+    if i <= 10:
+        cards.append(("red",i))
+    elif i <= 20:
+        cards.append(("black", i-10))
+    else:
+        cards.append(("yellow", i-20))
+
+pl1 = []
+pl2 = []
+
+for i in range(1,31):
+    card = cards.pop(randint(0,len(cards)-1))
+    if i % 2 == 0:
+        print("Player 2 draws")
+        pl2.append(card)
+        sleep(1)
+
+    else:
+        print("Player 1 draws")
+        pl1.append(card)
+        sleep(1)
+
+print(pl1, pl2)
