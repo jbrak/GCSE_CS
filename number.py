c7ceb8d404db80eb7498a12041696deec6e39a85
Jbrak <joe@brakoniecki.com>
2020-12-02 10:06:07 +0000

    Added error handling

--- number.py
+++ number.py
@@ -1,7 +1,11 @@
 num = 0
 
 while num < 10 or num > 20:
-    num = int(input("Enter a integer between 10&20:\n"))
+    try:
+        num = int(input("Enter a integer between 10&20:\n"))
+    except ValueError:
+        print("That was not an integer.\n")
+        continue
 
     if num > 20:
         print("Too High")
