0150e766ae40f0fb685d52e64c6afe912996ca9a
Jbrak <joe@brakoniecki.com>
2021-01-20 09:46:14 +0000

    Done Casting

---
+++ casting.py
@@ -0,0 +1,18 @@
+num = input("Enter a number")
+
+for i in num:
+    if i == ".":
+        try:
+            num = float(num)
+            print("Your number is a float.")
+        except ValueError:
+            print("That is not a number")
+        break
+
+
+if type(num) != float:
+    try:
+        num = int(num)
+        print("Your number is an integer")
+    except ValueError:
+        print("Thats not a number")
