c0a5815dc80b966f19946ce00df2677452cecf4d
Jbrak <joe@brakoniecki.com>
2021-10-06 10:07:34 +0100

    iso.py

---
+++ iso.py
@@ -0,0 +1,8 @@
+length1 = int(input("enter length1: "))
+length2 = int(input("enter length2: "))
+length3 = int(input("enter length3: "))
+
+if (length1 == length2) or (length1 == length3) or (length2 == length3):
+	print("Isosceles")
+else:
+    print("not isosceles")
