67bf2843032daf776b87abc18c88ffa7e2e55819
Jbrak <joe@brakoniecki.com>
2021-11-04 08:39:38 +0000

    ...

--- match.py
+++ match.py
@@ -3,5 +3,7 @@ country = input("Enter country: ")
 match country.lower():
     case "uk":
         print("britania rules the world")
+    case "usa":
+        print("we are not dumb")
     case _:
         print("losers")
