aa510b85ae6917f4c45e7e2ca567a35d30661572
Jbrak <joe@brakoniecki.com>
2021-12-02 11:11:11 +0000

    made more efficient

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,8 @@
+from math import ceil
+
 word = input("Enter a word: ").lower()
 
-for i in range(0,len(word)):
+for i in range(0,ceil(len(word)/2)):
     if word[i] == word[-i-1]:
         palindrome = True
     else:
