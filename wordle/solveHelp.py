b6d3691a700e40678cf10b6bda807b273312bbfd
Jbrak <joe@brakoniecki.com>
2022-01-20 11:12:21 +0000

    worksssss

--- wordle/solveHelp.py
+++ wordle/solveHelp.py
@@ -25,7 +25,7 @@ for j in letters:
 possibleWords1 = []
 
 for i in possibleWords:
-    if possibleWords.count(i) > 1 and possibleWords1.count(i) == 0:
+    if possibleWords.count(i) > (len(letters)-1) and possibleWords1.count(i) == 0:
         possibleWords1.append(i)
 
 possibleWords2 = []
@@ -41,7 +41,7 @@ for j in letters:
 possibleWords3 = []
 
 for i in possibleWords2:
-    if possibleWords2.count(i) > 1 and possibleWords3.count(i) == 0:
+    if possibleWords2.count(i) > (len(letters)-1) and possibleWords3.count(i) == 0:
         possibleWords3.append(i)
 
 print("3", possibleWords3)
